55f4614ad19d7e50d21963250ac1967a7758fe50
limbo <limbo_china@163.com>
2017-04-23 16:17:24 +0800

    update

--- src/atom.c
+++ src/atom.c
@@ -219,8 +219,8 @@ void adjustAtoms(struct SystemStr* sys){
             }   
             moveAtom(sys->cells, sys->atoms, count, nCell, nCell2);           
         }
-    MPI_Allreduce(&sys->atoms->myNum, &sys->atoms->totalNum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-    printTotalAtom(stdout,sys->atoms);
+    //MPI_Allreduce(&sys->atoms->myNum, &sys->atoms->totalNum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    //printTotalAtom(stdout,sys->atoms);
     //printf("adjust\n");
 
     // 与各邻居进程进行通信
